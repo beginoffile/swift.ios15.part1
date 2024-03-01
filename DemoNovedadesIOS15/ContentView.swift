981daedd35b05240b0a3dbb6ea59b8313fd3a4f6
cesar <cursos.mapatupisaro@gamail.com>
2024-03-01 17:59:34 -0600

    text fields

--- DemoNovedadesIOS15/ContentView.swift
+++ DemoNovedadesIOS15/ContentView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum Campos{
+        case nombre
+        case apellido
+        case email
+    }
+    
+    @State private var nombre = ""
+    @State private var apellido = ""
+    @State private var email = ""
+    @FocusState private var foco: Campos?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            TextField(text: $nombre) {
+                Text("Nombre")
+            }
+            .focused($foco, equals: .nombre)
+            .textFieldStyle(.roundedBorder)
+            .submitLabel(.continue)
+            
+            TextField(text: $apellido) {
+                Text("Apellido")
+            }
+            .focused($foco, equals: .apellido)
+            .textFieldStyle(.roundedBorder)
+            .submitLabel(.continue)
+            
+            TextField(text: $email) {
+                Text("Email")
+            }
+            .focused($foco, equals: .email)
+            .textFieldStyle(.roundedBorder)
+            .submitLabel(.done)
+            .keyboardType(.emailAddress)
+            
+            TextField("Name", text: $nombre)
+                .textFieldStyle(.roundedBorder)
+                .toolbar{
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "pencil.tip.crop.circle")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "tablecells")
+                        })
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "camera")
+                        })
+                        
+                        
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Registar")
+                        })
+                    }
+                }
+            
         }
         .padding()
+        .onSubmit {
+            switch foco{
+            case .nombre:
+                foco = .apellido
+            case .apellido:
+                foco = .email
+            default:
+                print("Guardar Formulario")
+            }
+        }
     }
 }
 
