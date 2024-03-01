15f10e294749ee0509620a415509a757d7c83130
cesar <cursos.mapatupisaro@gamail.com>
2024-03-01 17:22:35 -0600

    Initial Commit

---
+++ DemoNovedadesIOS15/DemoNovedadesIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoNovedadesIOS15App.swift
+//  DemoNovedadesIOS15
+//
+//  Created by Aguid Ramirez Sanchez on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoNovedadesIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
