668c90d79ebfed1d794f241a4e5319ccc5232f25
cesar <cursos.mapatupisaro@gamail.com>
2024-03-01 21:13:43 -0600

    add .refresable

---
+++ DemoNovedadesIOS15/RefreshView.swift
@@ -0,0 +1,18 @@
+//
+//  RefreshView.swift
+//  DemoNovedadesIOS15
+//
+//  Created by Aguid Ramirez Sanchez on 01/03/24.
+//
+
+import SwiftUI
+
+struct RefreshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RefreshView()
+}
